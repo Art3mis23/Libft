746abc1abd98fe195fbb6ef31228e54e7b8316f5
Maxence Roels <mroels@car12s1.42barcelona.com>
2024-07-03 01:14:04 +0200

    OMG I FUCKING FIGURED OUT MEMSET

--- ft_memset.c
+++ ft_memset.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maxenceroels <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 00:25:09 by maxencero         #+#    #+#             */
-/*   Updated: 2024/06/29 19:30:15 by mroels           ###   ########.fr       */
+/*   Updated: 2024/07/03 01:13:43 by mroels           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 void	*ft_memset(void *s, int c, size_t len)
 {
-	int	i;
-
-	i = 0;
-	while (i++ < len)
-	{	
-		*(char*)s = (unsigned char)c;
-		s++;
-	}
+	while (len--)
+		((unsigned char*)s)[len] = (unsigned char)c;
 	return (s);
 }
+
+/*int	main(void)
+{
+	char	string[5] = "Hell";
+	int	chat = 69;
+	size_t	lenght = 2;
+	ft_memset(string, chat, lenght);
+	return (0);
+}*/
