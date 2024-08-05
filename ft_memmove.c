651e2c5c5a03a7b246d2183f316e4bd1dce1d4cc
Maxence Roels <maxenceroels@MacBook-Pro-de-Maxence.local>
2024-08-05 12:52:44 +0200

    added error management to memmove (for null arguments but still has overlap problems

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mroels <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 01:44:58 by mroels            #+#    #+#             */
-/*   Updated: 2024/07/15 14:25:06 by mroels           ###   ########.fr       */
+/*   Updated: 2024/08/05 12:49:06 by maxencero        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	if (!dest && !src)
+		return (0);
 	while (n--)
 		((char *)dest)[n] = ((char *)src)[n];
 	return (dest);
