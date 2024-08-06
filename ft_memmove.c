4ad670f8adbe6d4d0c0cd24f144033f70237c3ab
Maxence Roels <mroels@car4s2.42barcelona.com>
2024-08-06 13:48:33 +0200

    oMG I FIXED MEMMOVE

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,17 +6,21 @@
 /*   By: mroels <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 01:44:58 by mroels            #+#    #+#             */
-/*   Updated: 2024/08/05 12:49:06 by maxencero        ###   ########.fr       */
+/*   Updated: 2024/08/06 13:45:32 by mroels           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	if (!dest && !src)
 		return (0);
-	while (n--)
-		((char *)dest)[n] = ((char *)src)[n];
+	if (src > dest)
+		ft_memcpy(dest, src, n);
+	else 
+		while (n--)
+			((char *)dest)[n] = ((char *)src)[n];
 	return (dest);
 }
