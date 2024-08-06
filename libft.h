833dbf691172704e5c498ab7c20ad2798f7022a2
Maxence Roels <mroels@car4s2.42barcelona.com>
2024-08-06 12:48:57 +0200

    norm issues fix in .h file

--- libft.h
+++ libft.h
@@ -6,26 +6,25 @@
 /*   By: mroels <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:19:52 by mroels            #+#    #+#             */
-/*   Updated: 2024/07/23 12:30:53 by mroels           ###   ########.fr       */
+/*   Updated: 2024/08/06 12:48:33 by mroels           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
-
-#include <string.h>
+# define LIBFT_H
+# include <string.h>
 
 void	ft_bzero(void *s, size_t n);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int	ft_strlen(char *s);
+int		ft_strlen(char *s);
 
 #endif
